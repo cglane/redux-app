10fd10dcce33343a2edb7379125aa0dfc6757aca
Charles Lane <charles@gooddonegreat.com>
2017-05-23 01:05:19 -0400

    favicon

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -13,133 +13,122 @@ export default {
   hobbies: [],
   loading: false,
   movies:[{
-poster_path: "/v5wAZwRqpGWmyAaaJ8BBHYuNXnj.jpg",
+poster_path: "/tvSlBzAdRE29bZe5yYWrJ2ds137.jpg",
 adult: false,
-overview: "After having successfully eluded the authorities for years, Hannibal peacefully lives in Italy in disguise as an art scholar. Trouble strikes again when he's discovered leaving a deserving few dead in the process. He returns to America to make contact with now disgraced Agent Clarice Starling, who is suffering the wrath of a malicious FBI rival as well as the media.",
-release_date: "2001-02-08",
+overview: "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+release_date: "1977-05-25",
 genre_ids: [
-80,
-18,
-53,
-27
+12,
+28,
+878
 ],
-id: 9740,
-original_title: "Hannibal",
+id: 11,
+original_title: "Star Wars",
 original_language: "en",
-title: "Hannibal",
-backdrop_path: "/uq53wVM1DLRmKlgLmIs60mBvlP3.jpg",
-popularity: 1.574072,
-vote_count: 1084,
-video: false,
-vote_average: 6.4
-},
-{
-poster_path: "/k1QRG9qZzuuJe4JTTpe4jlnQ5tt.jpg",
-adult: false,
-overview: "The story of the early, murderous roots of the cannibalistic killer, Hannibal Lecter – from his hard-scrabble Lithuanian childhood, where he witnesses the repulsive lengths to which hungry soldiers will go to satiate themselves, through his sojourn in France, where as a med student he hones his appetite for the kill.",
-release_date: "2007-02-06",
-genre_ids: [
-80,
-18,
-53
-],
-id: 1248,
-original_title: "Hannibal Rising",
-original_language: "en",
-title: "Hannibal Rising",
-backdrop_path: "/6FQAEoH21i5xM3swUuheaLk4Ipw.jpg",
-popularity: 1.535102,
-vote_count: 573,
-video: false,
-vote_average: 6
-},
-{
-poster_path: "/2JdF53YsOme6BEJi31HyXrZkj5.jpg",
-adult: false,
-overview: "A Carthaginian general attempts to cross the Alps with an army of elephants in order to conquer Rome.",
-release_date: "1959-12-21",
-genre_ids: [
-36
-],
-id: 169818,
-original_title: "Annibale",
-original_language: "it",
-title: "Hannibal",
-backdrop_path: "/pWXwXkXcxFOBI41BdyXHr8Bl3qV.jpg",
-popularity: 1.107912,
-vote_count: 2,
-video: false,
-vote_average: 5
-},
-{
-poster_path: "/4tT7BHvcH5xKF2gDpaa0jr66gh.jpg",
-adult: false,
-overview: "A POW in World War II is put to work in a Munich zoo, looking after an Asian elephant. The zoo is bombed by the Americans and the director of the zoo decides it is not safe for his Asian elephant Lucy to remain there. So he sends Brooks to safety with Lucy. They escape and go on the run in order to get to Switzerland.",
-release_date: "1969-04-30",
-genre_ids: [
-10752,
-35
-],
-id: 26972,
-original_title: "Hannibal Brooks",
-original_language: "en",
-title: "Hannibal Brooks",
-backdrop_path: "/xZ2dPjklFgBKnnWtLpVs5CC5pnA.jpg",
-popularity: 1.199303,
-vote_count: 3,
-video: false,
-vote_average: 7
-},
-{
-poster_path: "/mEwJZh8qnDqltQwMFNEqE1ilI7x.jpg",
-adult: false,
-overview: "A school teacher becomes a momentary hero after having rescued a stuffed-bird from a school incident.",
-release_date: "1956-10-18",
-genre_ids: [
-18
-],
-id: 254359,
-original_title: "Hannibál tanár úr",
-original_language: "hu",
-title: "Professor Hannibal",
-backdrop_path: null,
-popularity: 1.000498,
-vote_count: 2,
-video: false,
-vote_average: 5.5
-},
-{
-poster_path: "/6tmb1vGawOnUplq3Bz1caab2EJr.jpg",
-adult: false,
-overview: "No overview.",
-release_date: "1997-02-07",
-genre_ids: [
-35
-],
-id: 116357,
-original_title: "Hannibal & Jerry",
-original_language: "en",
-title: "Hannibal & Jerry",
-backdrop_path: null,
-popularity: 1.009567,
-vote_count: 4,
-video: false,
-vote_average: 4.6
-},
-{
-poster_path: "/uwKq3xnYcw18y79FOpoMKDPq0Pa.jpg",
-adult: false,
-overview: "Hannibal Buress braves Scotland's epic Fringe festival in Edinburgh, performing dozens of wry stand-up sets and testing new material on the locals.",
-release_date: "2016-04-08",
-genre_ids: [ ],
-id: 391899,
-original_title: "Hannibal Takes Edinburgh",
-original_language: "en",
-title: "Hannibal Takes Edinburgh",
-backdrop_path: "/qJ0MU3Ln21q7KqcM5UUeL9KjjMv.jpg",
-popularity: 1.02601,
-vote_count: 2,
+title: "Star Wars",
+backdrop_path: "/4iJfYYoQzZcONB9hNzg0J0wWyPH.jpg",
+popularity: 10.221142,
+vote_count: 5623,
 video: false,
 vote_average: 8
+},
+{
+poster_path: "/xPihqTMhCh6b8DHYzE61jrIiNMS.jpg",
+adult: false,
+overview: "The fate of the galaxy rests in the hands of bitter rivals. One, James Kirk, is a delinquent, thrill-seeking Iowa farm boy. The other, Spock, a Vulcan, was raised in a logic-based society that rejects all emotion. As fiery instinct clashes with calm reason, their unlikely but powerful partnership is the only thing capable of leading their crew through unimaginable danger, boldly going where no one has gone before. The human adventure has begun again.",
+release_date: "2009-05-06",
+genre_ids: [
+878,
+28,
+12
+],
+id: 13475,
+original_title: "Star Trek",
+original_language: "en",
+title: "Star Trek",
+backdrop_path: "/1XOSh6BFZbQ0xN75m4avqgzClyG.jpg",
+popularity: 4.820547,
+vote_count: 4044,
+video: false,
+vote_average: 7.4
+},
+{
+poster_path: "/mLrQMqyZgLeP8FrT5LCobKAiqmK.jpg",
+adult: false,
+overview: "The USS Enterprise crew explores the furthest reaches of uncharted space, where they encounter a mysterious new enemy who puts them and everything the Federation stands for to the test.",
+release_date: "2016-07-07",
+genre_ids: [
+28,
+12,
+878
+],
+id: 188927,
+original_title: "Star Trek Beyond",
+original_language: "en",
+title: "Star Trek Beyond",
+backdrop_path: "/6uBlEXZCUHM15UNZqNig17VdN4m.jpg",
+popularity: 7.34262,
+vote_count: 2063,
+video: false,
+vote_average: 6.5
+},
+{
+poster_path: "/hOsvkCePPJ9Fa673zov7sLXH0MI.jpg",
+adult: false,
+overview: "Three different persons, three different lives are connected in a mysterious way. 15 year old teenager suffering of misunderstanding, his glamorous and arrogant stepmother and the young untalented but full of optimism actress. Their destinies aren’t predetermined and their lives are very fragile.",
+release_date: "2014-06-18",
+genre_ids: [
+18,
+10749,
+35
+],
+id: 278718,
+original_title: "Звезда",
+original_language: "ru",
+title: "Star",
+backdrop_path: "/xLuFxFnYNotSovIVTV3QmfWnL8O.jpg",
+popularity: 1.09867,
+vote_count: 9,
+video: false,
+vote_average: 5.7
+},
+{
+poster_path: "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg",
+adult: false,
+overview: "A rogue band of resistance fighters unite for a mission to steal the Death Star plans and bring a new hope to the galaxy.",
+release_date: "2016-12-14",
+genre_ids: [
+878
+],
+id: 330459,
+original_title: "Rogue One: A Star Wars Story",
+original_language: "en",
+title: "Rogue One: A Star Wars Story",
+backdrop_path: "/tZjVVIYXACV4IIIhXeIM59ytqwS.jpg",
+popularity: 17.661214,
+vote_count: 3729,
+video: false,
+vote_average: 7.3
+},
+{
+poster_path: "/weUSwMdQIa3NaXVzwUoIIcAi85d.jpg",
+adult: false,
+overview: "Thirty years after defeating the Galactic Empire, Han Solo and his allies face a new threat from the evil Kylo Ren and his army of Stormtroopers.",
+release_date: "2015-12-15",
+genre_ids: [
+28,
+12,
+878,
+14
+],
+id: 140607,
+original_title: "Star Wars: The Force Awakens",
+original_language: "en",
+title: "Star Wars: The Force Awakens",
+backdrop_path: "/njv65RTipNSTozFLuF85jL0bcQe.jpg",
+popularity: 9.944555,
+vote_count: 6698,
+video: false,
+vote_average: 7.5
 }]
 };
